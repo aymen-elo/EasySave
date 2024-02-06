a3660da46efec82a71f9c08fb22852032a4dd5cc
ThéoWATTIER <theowattie@hotmail.fr>
2024-02-06 14:09:13 +0100

    Adding Library (Job, Menu, Save)

---
+++ Library/Job.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EasySave.Library
+{
+     class Job
+    {
+        private int id { get; set; } // voir fonction incrémentation auto
+        private string name { get; set; }
+        private string source { get; set; }
+        private string target { get; set; }
+        private int type { get; set; }
+        private int state { get; set; }
+        private int position { get; set; }
+        public Job() 
+        {
+            id = 0; // a modif 
+            name = string.Empty;
+            source = string.Empty;
+            target = string.Empty;
+            type = 0;
+            state = 0;
+            position = 0;
+        }
+        public bool begin(string name, string source, string target, int type) 
+        {
+            return 1; // si pas erreur lors lancement, renvoyé 1
+        }
+        public int getState(int id) 
+        {
+            return state;
+        }
+        public int getPosition(int id) 
+        {
+            return position;
+        }
+    }
+}
