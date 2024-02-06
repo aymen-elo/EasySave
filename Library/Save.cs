be9ce8b3e793fecb98929a0b25747e8163a082ac
ThéoWATTIER <theowattie@hotmail.fr>
2024-02-06 14:16:32 +0100

    Adding Library (Job, Menu, Save)

---
+++ Library/Save.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EasySave.Library
+{
+    class Save
+    {
+        //Ici définition Parametres & méthodes
+        public Save() //Définition constructeur
+        {
+            //
+        }
+    }
+}
