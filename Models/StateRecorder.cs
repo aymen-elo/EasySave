2d27a4eeba175b8ced716b25610d968d9bce5961
Perujan <perujan.kokilathasan@viacesi.fr>
2024-02-08 13:38:10 +0100

    add Logs

---
+++ Models/StateRecorder.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EasySave.Models
+{
+    public class StateRecorder
+    {
+        private readonly string stateFilePath = @"C:\temp\etat_avancement.txt";
+
+        public void RecordState(string state)
+        {
+            string stateMessage = $"{DateTime.Now:yyyy-MM-dd HH:mm:ss} - {state}";
+            File.WriteAllText(stateFilePath, stateMessage);
+        }
+    }
+}
