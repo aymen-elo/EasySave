f4c19e0c7c913480b97bcfa010791dcf396ca0e0
ThéoWATTIER <theowattie@hotmail.fr>
2024-02-21 10:58:52 +0100

    Add button on dgJobList

--- CryptoSoft/Program.cs
+++ CryptoSoft/Program.cs
@@ -8,33 +8,36 @@ class Program
     {
         Stopwatch stopwatch = new Stopwatch();
         stopwatch.Start();
-        TimeSpan timeElapsed = TimeSpan.Zero;
-        
-        
+
+
         string sourceFile = args[0];        //  [0] = source_file
         string fileName = Path.GetFileName(sourceFile);
         string targetFile = args[1];        //  [1] = target_file
         string encryptionKey = args[2];     //  [2] = encryption_key
-        
 
-        if (Path.GetExtension(fileName) == ".cry")
+        try
         {
-            targetFile = "C:\\Prosoft\\EasySave\\Destination" + "\\" + Path.GetFileNameWithoutExtension(fileName);       //  [1] = target_directory for decrypting
-            timeElapsed = DecryptFile(sourceFile, targetFile, encryptionKey, stopwatch);
+            int timeElapsed;
+            if (Path.GetExtension(fileName) == ".cry")
+            {
+                targetFile = targetFile + "\\" + Path.GetFileNameWithoutExtension(fileName);       //  [1] = target_directory for decrypting
+                timeElapsed = DecryptFile(sourceFile, targetFile, encryptionKey, stopwatch);
+            }
+            else
+            {
+                targetFile = targetFile + "\\" + fileName + ".cry";       //  [1] = target_directory for encrypting
+                timeElapsed = EncryptFile(sourceFile, targetFile, encryptionKey, stopwatch);
+            }
+            Console.WriteLine(timeElapsed);
         }
-        else
+        catch (Exception e)
         {
-            targetFile = "C:\\Prosoft\\EasySave\\Destination" + "\\" + fileName + ".cry";       //  [1] = target_directory for encrypting
-            timeElapsed = EncryptFile(sourceFile, targetFile, encryptionKey, stopwatch);
+            Console.WriteLine(1);
+            throw;
         }
-
-        Console.WriteLine(timeElapsed);
     }
     
-    
-    
-    
-    static TimeSpan EncryptFile(string inputFile, string outputFile, string key, Stopwatch stopwatch)
+    static int EncryptFile(string inputFile, string outputFile, string key, Stopwatch stopwatch)
     {
         using (FileStream inputStream = new FileStream(inputFile, FileMode.Open, FileAccess.Read))
         using (FileStream outputStream = new FileStream(outputFile, FileMode.Create, FileAccess.Write))
@@ -55,9 +58,10 @@ class Program
             }
         }
         stopwatch.Stop();
-        return stopwatch.Elapsed;
+        return stopwatch.Elapsed.Milliseconds;
     }
-    static TimeSpan DecryptFile(string inputFile, string outputFile, string key, Stopwatch stopwatch)
+    
+    static int DecryptFile(string inputFile, string outputFile, string key, Stopwatch stopwatch)
     {
         using (FileStream inputStream = new FileStream(inputFile, FileMode.Open, FileAccess.Read))
         using (FileStream outputStream = new FileStream(outputFile, FileMode.Create, FileAccess.Write))
@@ -77,7 +81,6 @@ class Program
             }
         }
         stopwatch.Stop();
-        return stopwatch.Elapsed;
+        return stopwatch.Elapsed.Milliseconds;
     }
-
 }
