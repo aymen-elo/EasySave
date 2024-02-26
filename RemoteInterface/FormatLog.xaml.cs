8113892b39dad666d41d356fa0e0404ea64710f1
ThéoWATTIER <theowattie@hotmail.fr>
2024-02-26 16:59:23 +0100

    Add Remote interface

---
+++ RemoteInterface/FormatLog.xaml.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections.Generic;
+using System.Runtime.CompilerServices;
+using System.Windows;
+using EasySaveLib.Model;
+
+namespace RemoteInterface
+{
+    public partial class FormatLog : Window
+    {
+        private Logger logger = Logger.GetInstance();
+
+        public bool IsJsonSelected { get; private set; }
+        public bool IsXmlSelected { get; private set; }
+        public string encryptionKey { get; private set; }
+        public string prioList { get; private set; }
+
+        public FormatLog()
+        {
+            InitializeComponent();
+            string logFormat = ConfigManager.GetLogFormat();
+            if (logFormat == "xml")
+            {
+                rbXml.IsChecked = true;
+            }
+            else if (logFormat == "json")
+            {
+                rbJson.IsChecked = true;
+            }
+            
+            tboxEncryptionKey.Text = ConfigManager.GetEncryptionKey();
+            tboxCipherList.Text = ConfigManager.GetCipherList();
+            tboxPrioList.Text = ConfigManager.GetPriorityList();
+            tboxBigFile.Text = ConfigManager.GetBigFileSize();
+
+        }
+
+        private void btnSave_Click(object sender, RoutedEventArgs e)
+        {
+            if (rbXml.IsChecked == true)
+            {
+                ConfigManager.SaveLogFormat("xml");
+                logger.LogFormat = "xml";
+                
+            }
+            else if (rbJson.IsChecked == true)
+            {
+                ConfigManager.SaveLogFormat("json");
+                logger.LogFormat = "json";
+            }
+
+            string BigFileSize = tboxBigFile.Text;
+            
+            ConfigManager.SaveEncryptionKey(tboxEncryptionKey.Text);
+            ConfigManager.SaveCipherList(tboxCipherList.Text);
+            ConfigManager.SavePriorityList(tboxPrioList.Text);
+            ConfigManager.SaveBigFileSize(tboxBigFile.Text);
+            
+
+
+            this.Close();
+        }
+    }
+}
---
+++ RemoteInterface/MainWindow.xaml.cs
@@ -0,0 +1,123 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using EasySaveLib.Model;
+
+// using EasySave.Model.Remote;
+// using EasySave_RemoteClient.src;
+
+namespace RemoteInterface
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private int progress;
+        private ResourceDictionary _languageDictionary;
+        
+        private async Task StartAsync()
+        {
+            
+        }
+        
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            clientSide();
+        }
+
+        private async Task clientSide()
+        {
+            Server server = new Server(); 
+            server.StartAsync(progress);
+            
+            Client client = new Client();
+            await client.ConnectAsync();
+        }
+        
+        
+        public MainWindow()
+        {
+            InitializeComponent();
+            btnRunJob.IsEnabled = false;
+            btnRemoveJob.IsEnabled = false;
+            _languageDictionary = new ResourceDictionary();
+        }
+
+        private void btnNewJob_Click(object sender, RoutedEventArgs e)
+        {
+            AddJobWindow addJobWindow = new AddJobWindow();
+            addJobWindow.ShowDialog();
+        }
+
+        private void btnOption_Click(object sender, RoutedEventArgs e)
+        {
+            // foreach (var j in _jobsViewModel.Jobs)
+            // {
+            //     j.NbSavedFiles = 100;
+            //     j.TotalFilesToCopy = 100;
+            // }
+            
+            FormatLog optionWindow = new FormatLog();
+            optionWindow.ShowDialog();
+        }
+        
+        
+        /* Language Management */
+        
+        //private void menuItemLang_Click(object sender, RoutedEventArgs e) { }
+        
+        
+        /* ******************* */
+
+        private void btnRunJob_Click(object sender, RoutedEventArgs e)
+        {
+            
+        }
+
+        private void btnRemoveJob_Click(object sender, RoutedEventArgs e)
+        {
+            
+        }
+
+        private void btnEditJob_Click(object sender, RoutedEventArgs e)
+        {
+        }
+
+        private void btnPlayPause_Click(object sender, RoutedEventArgs e)
+        {
+            
+        }
+
+        private void btnStopJob_Click(object sender, RoutedEventArgs e)
+        {
+        }
+
+        private void btnLogs_Click(object sender, RoutedEventArgs e)
+        {
+        }
+
+        private void dgJobList_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            Job job = (Job)(dgJobList.SelectedItem);
+            if (job != null)
+            {
+                btnRunJob.IsEnabled = true;
+                btnRemoveJob.IsEnabled = true;
+            }
+            else
+            {
+                btnRunJob.IsEnabled = false;
+                btnRemoveJob.IsEnabled = false;
+            }
+        }
+    }
+}
