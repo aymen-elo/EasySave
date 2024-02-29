eb7e38a088de2fbcce5ac7e107e2b5a7e2475371
ThéoWATTIER <theowattie@hotmail.fr>
2024-02-29 21:24:50 +0100

    Remote Edit OK

---
+++ EasySaveRemote/ViewModel/AddJobViewModel.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.IO;
+using System.Windows.Input;
+using EasySaveGUI.Command;
+using System.Windows.Forms;
+using EasySaveGUI.Controller;
+using EasySaveLib.Model;
+using EasySaveRemote.Packets;
+using Newtonsoft.Json;
+
+
+namespace EasySaveRemote.ViewModel
+{
+    public class AddJobViewModel : ViewModelBase
+    {
+        public event Action RequestClose;
+        
+        public ICommand AddJobCommand { get; set; }
+        private Logger _logger;
+        private CopyController _copyController = new CopyController();
+        private MainWindow _mainWindow;
+        
+        public string JobName { get; set; }
+        public string JobSource { get; set; }
+        public string JobTarget { get; set; }
+        public int JobTypeIdx { get; set; }
+        
+        private ObservableCollection<Job> _jobs;
+        
+        public AddJobViewModel(ObservableCollection<Job> jobs, MainWindow mainWindow)
+        {
+            _logger = Logger.GetInstance();
+            _jobs = jobs;
+            _mainWindow = mainWindow;
+
+            AddJobCommand = new RelayCommand(AddJob);
+            
+            /* Job details from the Edit Window */
+
+        }
+        
+        private void AddJob(object obj)
+        {
+            Job _job = new Job(JobName, JobTypeIdx == 0 ? BackupType.Full : BackupType.Diff, JobSource, JobTarget);
+            
+            string jobJson = JsonConvert.SerializeObject(_job);
+
+            
+            SendMessage.SendMessageTo("127.0.0.1", 13,jobJson , MessageType.NJ, _mainWindow);
+
+            RequestClose?.Invoke();
+        }
+        
+        private void UpdateJobData(string newName, Job job)
+        {
+            // helpers to calculate the new directory's size info
+            DirectoryInfo diSource = new DirectoryInfo(job.SourceFilePath);
+            long totalFilesSize = _copyController._fileGetter.DirSize(diSource);
+            int totalFilesToCopy = _copyController._fileGetter.GetAllFiles(job.SourceFilePath).Count;
+                
+            job.TotalFilesSize = totalFilesSize;
+            job.NbFilesLeftToDo = totalFilesToCopy;
+            job.TotalFilesToCopy = totalFilesToCopy;
+            job.NbSavedFiles = 0;
+
+            _logger.LogState(job.Name, job.BackupType, job.SourceFilePath, job.TargetFilePath, job.State, job.TotalFilesToCopy, job.TotalFilesSize , (job.TotalFilesToCopy - job.NbSavedFiles), ((job.NbSavedFiles * 100) / job.TotalFilesToCopy), newName);
+        }
+    }
+}
---
+++ EasySaveRemote/ViewModel/EditJobViewModel.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.IO;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Input;
+using EasySaveGUI.Command;
+using EasySaveLib.Model;
+using EasySaveRemote.Packets;
+using Newtonsoft.Json;
+
+namespace EasySaveRemote.ViewModel
+{
+    public class EditJobViewModel : ViewModelBase
+    {
+        public event Action RequestClose;
+
+        public ICommand EditJobCommand { get; set; }
+        private Job _job;
+        private Logger _logger;
+        private MainWindow _mainWindow;
+
+        public string JobName { get; set; }
+        public string JobSource { get; set; }
+        public string JobTarget { get; set; }
+        public int JobTypeIdx { get; set; }
+
+        public EditJobViewModel(Job job, MainWindow mainWindow)
+        {
+            _job = job;
+            _logger = Logger.GetInstance();
+            _mainWindow = mainWindow;
+
+            /* Job details from the Edit Window */
+            JobName = job.Name;
+            JobSource = job.SourceFilePath;
+            JobTarget = job.TargetFilePath;
+            JobTypeIdx = job.BackupType == BackupType.Full ? 0 : 1;
+
+            EditJobCommand = new RelayCommand(EditJob);
+        }
+
+        private void EditJob(object parameter)
+        {
+            var newName = JobName;
+            try
+            {
+                if (!Directory.Exists(JobSource) || !Directory.Exists(JobTarget))
+                {
+                    throw new FileNotFoundException("Source file or target file doesn't exist.");
+                }
+
+                _job.SourceFilePath = JobSource;
+                _job.TargetFilePath = JobTarget;
+                _job.BackupType = JobTypeIdx == 0 ? BackupType.Full : BackupType.Diff;
+
+                _logger.LogState(_job, newName);
+                string oldName = _job.Name;
+                _job.Name = newName; // Search by Name => Update Name later on
+
+                string oldNameJson = JsonConvert.SerializeObject(oldName);
+                string jobJson = JsonConvert.SerializeObject(_job);
+                
+                string combinedJson = "[{\"OldName\":" + oldNameJson + "}," + jobJson + "]";
+                
+                
+                SendMessage.SendMessageTo("127.0.0.1", 13,combinedJson , MessageType.EJ, _mainWindow); 
+
+                
+                RequestClose?.Invoke();
+            }
+            catch (FileNotFoundException ex)
+            {
+                MessageBox.Show($"Error : {ex.Message}", "Error", MessageBoxButton.OK, MessageBoxImage.Error);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"There is an error : {ex.Message}", "Error", MessageBoxButton.OK,
+                    MessageBoxImage.Error);
+            }
+        }
+    }
+}
